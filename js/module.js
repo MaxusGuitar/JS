dc15aa9404edfc6f197d75e96f0976c68ae53e02
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-06-23 23:09:49 +0300

    и54т6и54т6и54т6

--- js/module.js
+++ js/module.js
@@ -1,2 +1,9 @@
 var x = 1
 console.log("hello", x)
+
+// let go = prompt()
+// console.log(`получай свой ответ:` ,go);
+console.log(Math.pow(2, 102));
+
+let r = Math.random() * 100
+console.log(Number.parseInt(r));
